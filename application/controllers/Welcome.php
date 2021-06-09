afc32dae8fc0da852f5f1c06c386c141f663938b
KresnaAP <kresnaadhipramana5758@gmail.com>
2021-06-09 21:37:28 +0700

    done

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,18 +18,57 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('student_model');
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data["students"] = $this->student_model->getAll();
+		$this->load->view('detail', $data);
 	}
 
-	public function about_us()
+	public function addStudent()
 	{
-		$this->load->view('about_us');
+		$student = $this->student_model;
+        $validation = $this->form_validation;
+        $validation->set_rules($student->rules());
+
+        if ($validation->run()) {
+            $student->create();
+            $this->session->set_flashdata('success', 'Berhasil disimpan');
+        }
+		$this->load->view('add_student');
 	}
 
-	public function contact_us()
-	{
-		$this->load->view('contact_us');
-	}
+	// public function editStudent($id = null)
+    // {
+    //     if (!isset($id)) redirect('admin/products');
+       
+    //     $product = $this->product_model;
+    //     $validation = $this->form_validation;
+    //     $validation->set_rules($product->rules());
+
+    //     if ($validation->run()) {
+    //         $product->update();
+    //         $this->session->set_flashdata('success', 'Berhasil disimpan');
+    //     }
+
+    //     $data["product"] = $product->getById($id);
+    //     if (!$data["product"]) show_404();
+        
+    //     $this->load->view("admin/product/edit_form", $data);
+    // }
+
+    // public function deleteStudent($id=null)
+    // {
+    //     if (!isset($id)) show_404();
+        
+    //     if ($this->product_model->delete($id)) {
+    //         redirect(site_url('admin/products'));
+    //     }
+    // }
 }
